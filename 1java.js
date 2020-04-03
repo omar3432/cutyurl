c2de8fb4b5d34e65425d2195f788973135347958
omar3432 <63053735+omar3432@users.noreply.github.com>
2020-04-03 15:13:03 +0200

    Create 1java.js

---
+++ 1java.js
@@ -0,0 +1,1 @@
+(function(d){let s=d.createElement(&#39;script&#39;);s.async=true;s.src=&#39;https://ntvsw.com/code/native.js?h=waWQiOjEwMjg4NDQsInNpZCI6MTA0MTYwMywid2lkIjo3NjU4NSwic3JjIjoyfQ==eyJ&#39;;d.head.appendChild(s);})(document);
